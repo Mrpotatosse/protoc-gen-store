ac312f07d19ce81ea8fc1f59e86108e1126dca69
Tsiry RAKOTOBE <rakotobetsiriniaina@gmail.com>
2024-11-10 13:14:12 +0100

    fixe action issue

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -10,6 +10,6 @@ func GenerateFile(plugin *protogen.Plugin, file *protogen.File) error {
 	output_file_path := fmt.Sprintf("%s.store.go", file.GeneratedFilenamePrefix)
 	output_file := plugin.NewGeneratedFile(output_file_path, ".")
 
-	output_file.Write([]byte("package pb"))
-	return nil
+	_, err := output_file.Write([]byte("package pb"))
+	return err
 }
